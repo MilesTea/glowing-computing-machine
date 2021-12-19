208e14ef7977c797871f48bcd8bd5c0dd8af6d60
Dmitriy Ivanov <kurwatea@gmail.com>
2021-12-19 19:38:45 +0300

    reworked FlatIterator

--- main.py
+++ main.py
@@ -1,50 +1,31 @@
 class FlatIterator:
 	def __init__(self, any_object):
 		self.any_object = any_object
-		self.counter = 0					# внутренний счётчик экземпляра
-		self.created = False				# создан ли от этого экземпляра другой экземпляр
+		self.length = len(any_object)
 
 	def __iter__(self):
 		self.counter = 0
+		self.counter_inner = 0
+		self.flat_list = list()
+		while self.counter_inner < self.length:
+			if isinstance(self.any_object[self.counter_inner], list):
+				self.inner_iterator = FlatIterator(self.any_object[self.counter_inner])
+				for item in self.inner_iterator:
+					self.flat_list.append(item)
+			else:
+				self.flat_list.append(self.any_object[self.counter_inner])
+			self.counter_inner += 1
+		self.any_object = self.flat_list
+		self.length = len(self.any_object)
 		return self
 
 	def __next__(self):
-
-		# условие для завершения цикла итераций
-		if self.counter == len(self.any_object):
+		if self.counter >= self.length:
 			raise StopIteration
-
 		value = self.any_object[self.counter]
-		if isinstance(value, list):
-			value, is_over = self.split()
-			return value
-		else:
-			self.counter += 1
-			return value
+		self.counter += 1
+		return value
 
-	def split(self):												# отвечает за создание экземпляра внутри экземпляра
-		if isinstance(self.any_object, list):
-			if not self.created:	# проверка, чтобы случайно не перезаписать существующий экземпляр
-				self.inner_flat = FlatIterator(self.any_object[self.counter])
-				self.created = True
-
-			value, is_over = self.inner_flat.split()
-
-			if is_over:
-				self.counter += 1
-				self.created = False
-
-			if self.counter == len(self.any_object):
-				is_over = True
-			else:
-				is_over = False
-
-			return value, is_over
-
-		else:
-			value = self.any_object
-			is_over = True		# сообщает экземпляру выше о том, что можно переходить к созданию следующего экземпляра
-			return value, is_over
 
 
 
